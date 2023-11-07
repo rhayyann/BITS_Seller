6ce8044841d94acf837be02d51f3f4124be3e890
hanan-achmad <hananachmad8@gmail.com>
2023-11-07 17:17:39 +0800

    global css setup

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,10 +1,28 @@
 import React from 'react'
+import { Container, Row, Col} from 'reactstrap'
 
 const Footer = () => {
+
     return (
-        <div>
-           Footer 
-        </div>
+        <footer className="footer">
+            <Container>
+                <Row>
+                    <Col>
+                    <div className='logo'>
+                            <div>
+                            <h1>BITS</h1>
+                            </div>
+                        </div>
+                    </Col>
+               
+                    <Col>
+                        <p className="footer_copyright">
+                            Copyright by BITS. All rights reserved.
+                        </p>
+                    </Col>
+                </Row>
+            </Container>
+        </footer>
     )
 }
 
