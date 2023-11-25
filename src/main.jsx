5a437d924f65211579ae4f45645b7a6ee68c0817
hanan-achmad <hananachmad8@gmail.com>
2023-11-26 02:29:15 +0800

    toastify setup

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,21 @@ import ReactDOM from "react-dom/client";
 
 import App from "./App";
 import {BrowserRouter} from 'react-router-dom';
+import { ToastContainer } from "react-toastify";
+import 'react-toastify/dist/ReactToastify.css'
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
+    <ToastContainer
+        theme="light"
+        position="top-right"
+        autoClose={3000}
+        closeOnClick
+        rtl={false}
+        pauseOnHover={false}
+      />
       <App />
     </BrowserRouter>
   </React.StrictMode>
