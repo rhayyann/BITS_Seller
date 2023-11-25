507088ec750863b095019a55ea1ffdf20b1f0f4f
hanan-achmad <hananachmad8@gmail.com>
2023-11-26 02:30:48 +0800

    fitur login dan signup

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,33 +1,110 @@
-import React from 'react'
+import React, {useState} from 'react'
 import '../styles/Login.css'
-import { useNavigate } from 'react-router-dom'
+import {useNavigate } from 'react-router-dom'
+import { signInWithEmailAndPassword, GoogleAuthProvider, signInWithPopup, updateProfile} from 'firebase/auth'
+import { auth } from '../firebase.config'
+import {setDoc, doc} from "firebase/firestore"
+import {toast} from 'react-toastify'
+import { db } from '../firebase.config'
+import GoogleButton from "react-google-button";
 
 const Login = () => {
+
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('');
+    const [loading, setLoading] = useState(false);
     const navigate = useNavigate();
+    
+    const signIn = async (e) =>{
+
+        e.preventDefault()
+        setLoading(true)
+
+        try{
+
+            const userCredential = await signInWithEmailAndPassword(auth, email, password)
+            const user = userCredential.user
+
+            console.log(user)
+            setLoading(false)
+            toast.success('Berhasil Login')
+            navigate('/checkout')
+
+        } catch (error) {
+            setLoading(false)
+            toast.error('Email atau Password Salah!')
+        }
+    }
+
+    const handleGoogleSignIn = async (e) => {
+        e.preventDefault();
+    
+        try {
+            const googleAuthProvider = new GoogleAuthProvider();
+
+            const userCredential = await signInWithPopup(auth, googleAuthProvider);
+            const user = userCredential.user
+
+            // update user
+            await updateProfile(user, {
+                displayName: user.email,
+            });
+
+            // simpan data user di firestore database
+            await setDoc(doc(db, 'users', user.uid), {
+                uid: user.uid,
+                email: user.email,
+            })
+            
+            console.log(user)
+            setLoading(false)
+            toast.success('Berhasil Login')
+            navigate('/checkout');
+        } catch (error) {
+            setLoading(false)
+            toast.error(error.message);
+        }
+      };
+
     return (
         <section className="login-section">
-            <div className="login-container">
-                <div className="content-left">
-                    <form action="#" className="login-form">
-                        <h1>LOGIN</h1>
-                        <div className="login-input">
-                            <input type="username" placeholder='Username'/>
-                            <input type="password" placeholder='Password' />
-                            <button>LOGIN</button>
-                        </div>
-                        <p className='option-label'>OR</p>
-                        <div className="login-google">
-                            <p>Google</p>
-                            <img src="google.png" alt="" />
-                        </div>
-                    </form>
+           {
+            loading? (
+                <h5>Loading...</h5> 
+               ):(
+                <div className="login-container">
+                    <div className="content-left">
+                        <form onSubmit={signIn} className="login-form">
+                            <h1>LOGIN</h1>
+                            <div className="login-input">
+                                <input type="email" placeholder='Email'
+                                value={email} onChange={e => setEmail(e.target.value)}/>
+    
+                                <input type="password" placeholder='Password' 
+                                value={password} onChange={e => setPassword(e.target.value)}/>
+                                
+                                <button type="submit" className="auth_btn">LOGIN</button>
+                            </div>
+    
+                            <p className='option-label'>OR</p>
+                            <div className="login-google">
+                                <GoogleButton
+                                 label='Login dengan Google '
+                                 className="g-btn"
+                                 type="light"
+                                 onClick={handleGoogleSignIn}
+                                />
+                            </div>
+                        </form>
+                    </div>
+                    <div className="content-right">
+                        <h1>CREATE AN ACCOUNT</h1>
+                        <p>It's easy. Enter your email address, fill in the form on the next page and enjoy the benefits of getting an account.</p>
+                        <button onClick={()=>navigate("/register")}>REGISTER</button>
+                    </div>
                 </div>
-                <div className="content-right">
-                    <h1>CREATE AN ACCOUNT</h1>
-                    <p>It's easy. Enter your email address, fill in the form on the next page and enjoy the benefits of getting an account.</p>
-                    <button onClick={()=>navigate("/register")}>REGISTER</button>
-                </div>
-            </div>
+               )
+           }
         </section>
     )
 }
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,32 +1,88 @@
-import React from 'react'
+import React, {useState} from 'react'
 import '../styles/Register.css'
-import { useNavigate } from 'react-router-dom'
+import { useNavigate} from 'react-router-dom'
+import {createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
+import {auth} from '../firebase.config'
+import {setDoc, doc} from "firebase/firestore"
+import {toast} from "react-toastify"
+import { db } from '../firebase.config'
 
 function Register() {
+
+    const [username, setUsername] = useState('')
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const [loading, setLoading] = useState(false)
+
     const navigate = useNavigate();
+
+    const signup = async(e) => {
+        e.preventDefault()
+        setLoading(true)
+
+        try {
+            const userCredential = await createUserWithEmailAndPassword(
+                auth, 
+                email, 
+                password
+            );
+            const user = await userCredential.user;
+
+            // update user
+            await updateProfile(user, {
+                displayName: username,
+              });
+
+            // simpan data user di firestore database
+            await setDoc(doc(db, 'users', user.uid ), {
+                uid: user.uid,
+                displayName: username,
+                email,
+                password
+              })
+
+            setLoading(false)
+            toast.success('Akun Berhasil Dibuat')
+            navigate('/login')
+
+            }catch (eror){
+              setLoading(false)
+
+              if (eror.code === 'auth/email-already-in-use') {
+                toast.error('Email sudah digunakan. Gunakan email lain!');
+              } else if (eror.code === 'auth/weak-password') {
+                toast.error('Password harus memiliki setidaknya 6 karakter!');
+              } else {
+                toast.error(eror.message);
+              }
+            };
+          };
+
+
   return (
     <section className="register-section">
-        <div className="register-container">
+      {
+        loading? (<h5>Loading...</h5> 
+        ) : (
+            <div className="register-container">
             <div className="content-left">
-                <form action="#" className="register-form">
+                <form className="register-form" onSubmit={signup} >
                     <h1>REGISTER</h1>
                     <div className="register-input">
                         {/* <input type="username" placeholder='Username'/> */}
-                        <h3>YOUR NAME</h3>
-                        <input type="text" placeholder='First Name' />
-                        <input type="text" placeholder='Last Name' />
-                        <input type="text" placeholder='Phone Number' />
+                        <h3>USERNAME</h3>
+                        <input type="text" placeholder='Username' 
+                         value={username} onChange={e => setUsername(e.target.value)}/>
+
                         <h3>LOGIN DETAILS</h3>
-                        <input type="email" placeholder='Email Address' />
-                        <input type="password" placeholder='Password' />
-                        <input type="password" placeholder='Confirm Password' />
+                        <input type="email" placeholder='Email Address' 
+                        value={email} onChange={e => setEmail(e.target.value)}/>
+
+                        <input type="password" placeholder='Password' 
+                         value={password} onChange={e => setPassword(e.target.value)}/>
+
                         <p></p>
-                        <button>REGISTER</button>
-                    </div>
-                    <p className='option-label'>OR</p>
-                    <div className="register-google">
-                        <p>Google</p>
-                        <img src="google.png" alt="" />
+                        <button  type="submit" className="auth_btn">REGISTER</button>
                     </div>
                 </form>
             </div>
@@ -35,6 +91,8 @@ function Register() {
                 <button onClick={()=>navigate("/login")}>LOGIN</button>
             </div>
         </div>
+        )
+      }
     </section>
   )
 }
